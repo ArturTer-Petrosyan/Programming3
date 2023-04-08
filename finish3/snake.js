53478bef814bba4cc74f8b216797042f328dfeff
ArturTer-Petrosyan <artur.t@tumo.org>
2023-04-08 12:39:52 +0400

    JavaScript Class Inheritance 2

--- finish3/snake.js
+++ finish3/snake.js
@@ -1,7 +1,6 @@
-class Snake {
+class Snake extends LivingCreature {
     constructor(x, y) {
-        this.x = x
-        this.y = y
+      super(x,y)
         this.energy = 15
         this.directions = [];
     }
